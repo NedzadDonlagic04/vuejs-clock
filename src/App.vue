52d30d761184f4d118cc95a752f6a3b35f68e347
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-05 01:36:59 +0200

    Added clock legs time tracking

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,64 @@
 <script>
 export default {
+    mounted() {
+        this.updateClock();
+        setInterval(this.updateClock, 1000);
+    },
+    data() {
+        return {
+            date: new Date()
+        }
+    },
+    methods: {
+        updateClock() {
+            this.date = new Date();
+        }
+    },
+    computed: {
+        getHours() {
+            const hours = this.date.getHours();
 
+            if(hours >= 12)
+            {
+                hours -= 12;
+            }
+
+            const degPerHour = 360 / 12;
+            const degOffset = -180;
+
+            const hoursDeg = degPerHour * hours + degOffset;
+
+            return `rotate(${hoursDeg}deg)`;
+        },
+        getMinutes() {
+            const minutes = this.date.getMinutes();
+
+            const degPerMinute = 360 / 60;
+            const degOffset = -180;
+
+            const minutesDeg = degPerMinute * minutes + degOffset;
+
+            return `rotate(${minutesDeg}deg)`;
+        },
+        getSeconds() {
+            const seconds = this.date.getSeconds();
+
+            const degPerSecond = 360 / 60;
+            const degOffset = -180;
+
+            const secondsDeg = degPerSecond * seconds + degOffset;
+
+            return`rotate(${secondsDeg}deg)`;
+        }
+    }
 }
 </script>
 
 <template>
     <div class="ball"></div>
-    <div class="leg hour"></div>
-    <div class="leg minute"></div>
-    <div class="leg second"></div>
+    <div class="leg hour" :style="{ transform: getHours}"></div>
+    <div class="leg minute" :style="{ transform: getMinutes}"></div>
+    <div class="leg second" :style="{ transform: getSeconds}"></div>
 </template>
 
 <style scoped>
